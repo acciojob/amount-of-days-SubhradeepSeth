255ba5065625368d1b90d2a28fe650471fc6c1dd
SubhradeepSeth <139810175+SubhradeepSeth@users.noreply.github.com>
2023-12-08 17:00:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let r=prompt();
+function daysOfAYear(r){
+	if(r%4===0 && r%100!==0) return 366;
+	else if(r%400==0) return 366;
+	else return 365;
+}
+alert(daysOfAYear(r));
+
+
